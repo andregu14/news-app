24171c5277406fd37c4ec6e1977d4e453b0c94d3
André Gustavo <andregustavo147@gmail.com>
2025-04-10 17:07:49 -0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { ScrollView, StyleSheet } from "react-native";
+import { Text, View } from "@/components/Themed";
+import Header from "@/components/Header";
+import { StatusBar } from "expo-status-bar";
+import SearchBar from "@/components/SearchBar";
+import HighlightCard from "@/components/HighlightCard";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
+      <Header style={styles.header} />
+      <SearchBar style={styles.searchBar} />
+      <View style={styles.carrousel}>
+        <Text style={styles.carrouselText}>🔥 Em alta</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} scrollToOverflowEnabled={true}>
+          <HighlightCard key={1} />
+          <HighlightCard key={2} />
+          <HighlightCard key={3} />
+        </ScrollView>
+      </View>
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -15,12 +27,22 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  header: {
+    marginTop: 45,
+  },
+  searchBar: {
+    marginTop: 30,
+    alignSelf: "center",
+    width: "90%",
+  },
+  carrousel: {
+    marginTop: 60,
+    paddingLeft: 10
+  },
+  carrouselText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
