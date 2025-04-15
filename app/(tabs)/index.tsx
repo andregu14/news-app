7af03d84ff8fc19fdf80f66e82bf9710c3ee3e37
André Gustavo <andregustavo147@gmail.com>
2025-04-15 16:58:47 -0300

    Update index.tsx
    Add data array placeholder,  update render logic for every type of card, update styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,20 +4,76 @@ import Header from "@/components/Header";
 import { StatusBar } from "expo-status-bar";
 import SearchBar from "@/components/SearchBar";
 import HighlightCard from "@/components/HighlightCard";
+import NewsCard from "@/components/NewsCard";
+
+type DataType = {
+  title: string;
+  description: string;
+  image: string;
+};
+
+const data: DataType[] = [
+  {
+    title: "Title 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: `https://picsum.photos/400/180?random=${Math.random()}`, // Uses Math.random() to generate a random image for each card
+  },
+  {
+    title: "Title 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: `https://picsum.photos/400/180?random=${Math.random()}`,
+  },
+  {
+    title: "Title 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: `https://picsum.photos/400/180?random=${Math.random()}`,
+  },
+];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      {/* Header Menu */}
       <Header style={styles.header} />
-      <SearchBar style={styles.searchBar} />
-      <View style={styles.carrousel}>
-        <Text style={styles.carrouselText}>🔥 Em alta</Text>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} scrollToOverflowEnabled={true}>
-          <HighlightCard key={1} />
-          <HighlightCard key={2} />
-          <HighlightCard key={3} />
-        </ScrollView>
-      </View>
+      <ScrollView>
+        {/* Search Bar */}
+        <SearchBar style={styles.searchBar} />
+        {/* Carrousel */}
+        <View style={styles.carrousel}>
+          <Text style={styles.carrouselText}>🔥 Em alta</Text>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            scrollToOverflowEnabled={true}
+          >
+            {data.map((item, index) => {
+              return (
+                <HighlightCard
+                  key={index}
+                  description={item.description}
+                  image={item.image}
+                />
+              );
+            })}
+          </ScrollView>
+        </View>
+        {/* Body */}
+        <View style={styles.body}>
+          {data.map((item, index) => {
+            return (
+              <NewsCard
+                key={index}
+                title={item.title}
+                bodyText={item.description}
+                image={item.image}
+              />
+            );
+          })}
+        </View>
+      </ScrollView>
 
       <StatusBar style="auto" />
     </View>
@@ -38,11 +94,14 @@ const styles = StyleSheet.create({
   },
   carrousel: {
     marginTop: 60,
-    paddingLeft: 10
   },
   carrouselText: {
     fontSize: 18,
     fontWeight: "bold",
+    marginLeft: 15,
     marginBottom: 20,
+  },
+  body: {
+    marginTop: 40,
   },
 });
