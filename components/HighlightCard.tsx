3ee167010d2a8357cad8997aec0c12de6977e4ca
André Gustavo <andregustavo147@gmail.com>
2025-04-15 16:56:30 -0300

    Update HighlightCard.tsx
    Style adjustment

--- components/HighlightCard.tsx
+++ components/HighlightCard.tsx
@@ -18,10 +18,10 @@ export default function HighlightCard({
     <View style={[styles.container, style, { borderColor: colorScheme === "dark" ? "rgba(255, 255, 255, 0.2)" : "#eee" }]}>
       <Image
         style={styles.imageContainer}
-        source={{ uri: image || randomImage }}
+        source={{ uri: image ?? randomImage }}
         resizeMode="cover"
       />
-      <Text style={styles.descriptionText}>{description ? description : "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"}</Text>
+      <Text ellipsizeMode={"tail"} numberOfLines={4} style={styles.descriptionText}>{description ? description : "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"}</Text>
     </View>
   );
 }
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     width: cardWidth,
     borderRadius: 14,
     alignItems: "center",
-    marginRight: 15,
+    marginHorizontal: 7.5,
     borderWidth: 1,
   },
   imageContainer: {
