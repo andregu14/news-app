fb7178445ad5307fd444298497204233e4810a2f
André Gustavo <andregustavo147@gmail.com>
2025-04-16 14:51:10 -0300

    New colors definition

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,13 +7,17 @@ export default {
     background: '#fff',
     tint: tintColorLight,
     tabIconSelected: tintColorLight,
-    headerIcon: "#3F3F3F"
+    headerIcon: "#3F3F3F",
+    borderColor: '#eee', 
+    secondaryText: '#888', 
   },
   dark: {
     text: '#fff',
     background: '#000',
     tint: tintColorDark,
     tabIconSelected: tintColorDark,
-    headerIcon: "#fff"
+    headerIcon: "#fff",
+    borderColor: 'rgba(255, 255, 255, 0.2)', 
+    secondaryText: 'rgba(255, 255, 255, 0.7)', 
   },
 };
