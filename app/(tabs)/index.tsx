6c07fd5cb1e3283a50877dcbaca0d02c59944158
André Gustavo <andregustavo147@gmail.com>
2025-06-04 18:16:20 -0300

    chore: change flatList and carousel to separate components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,16 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
-import {
-  StyleSheet,
-  FlatList,
-  ScrollView,
-  useColorScheme,
-  RefreshControl,
-  Dimensions,
-} from "react-native";
-import { TitleText, View } from "@/components/Themed";
+import { StyleSheet, useColorScheme, Dimensions } from "react-native";
+import { View } from "@/components/Themed";
 import Header from "@/components/Header";
 import { StatusBar } from "expo-status-bar";
-import SearchBar from "@/components/SearchBar";
-import HighlightCard from "@/components/HighlightCard";
-import NewsCard from "@/components/NewsCard";
 import { useRouter } from "expo-router";
 import { newsData, ArticleParams } from "@/constants/NewsData";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import HighlightCardSkeleton from "@/components/HighlightCardSkeleton";
 import Colors from "@/constants/Colors";
-import NewsCardSkeleton from "@/components/NewsCardSkeleton";
 import SideMenu from "@/components/SideMenu";
 import AccountSideMenu from "@/components/AccountSideMenu";
+import NewsList from "@/components/NewsList";
+import { fetchNewsAPI } from "@/services/newsAPI";
+import { formatTimeAgo } from "@/utils/dateFormat";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import ReanimatedDrawerLayout, {
   DrawerType,
@@ -31,8 +21,7 @@ import AboutUsModal from "@/components/AboutUsModal";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { useDispatch } from "react-redux";
 import { setQuery } from "@/store/searchSlice";
-import { formatDistanceToNowStrict } from "date-fns";
-const { ptBR } = require("date-fns/locale");
+import NewsListHeader from "@/components/NewsListHeader";
 
 const { width: screenWidth } = Dimensions.get("window");
 const drawerWidth = screenWidth * 0.8;
@@ -51,6 +40,7 @@ export default function Index() {
   const rightDrawerRef = useRef<DrawerLayoutMethods>(null);
   const aboutUsRef = useRef<BottomSheetModal>(null);
 
+  // const apiKey = "f2abf5755e8f5f3db39550a53a4daa18";
   const apiKey = "aaaa";
 
   // Função para buscar notícias
@@ -60,55 +50,32 @@ export default function Index() {
     } else {
       setIsRefreshing(true);
     }
+
     // Delay para refresh e loading
     const delay = refresh
       ? 500
       : Math.floor(Math.random() * (4000 - 2000 + 1) + 2000);
+
     setTimeout(async () => {
       try {
-        const response = await fetch(
-          `https://gnews.io/api/v4/top-headlines?country=br&apikey=${apiKey}`
-        );
-        if (!response.ok) throw new Error("Falha ao buscar notícias");
-        else console.log("Dados de todas as noticias extraidos com sucesso!");
-        const data = await response.json();
+        const data = await fetchNewsAPI("", apiKey);
+        console.log("Dados de todas as notícias extraídos com sucesso!");
 
         // Formatar as datas para "ha x tempo"
-        const formattedArticles = data.articles.map(
-          (article: ArticleParams) => {
-            const timeAgo = formatDistanceToNowStrict(
-              new Date(article.publishedAt),
-              {
-                addSuffix: true,
-                locale: ptBR,
-              }
-            );
-            return {
-              ...article,
-              publishedAt: timeAgo.replace("há", "Há"),
-            };
-          }
-        );
+        const formattedArticles = data.articles.map((article) => ({
+          ...article,
+          publishedAt: formatTimeAgo(article.publishedAt),
+        }));
 
         setNews(formattedArticles);
       } catch (error) {
         console.error("Erro ao buscar notícias, usando dados locais:", error);
-        // Formatar as datas para "ha x tempo"
-        const formattedArticles = newsData.articles.map(
-          (article: ArticleParams) => {
-            const timeAgo = formatDistanceToNowStrict(
-              new Date(article.publishedAt),
-              {
-                addSuffix: true,
-                locale: ptBR,
-              }
-            );
-            return {
-              ...article,
-              publishedAt: timeAgo.replace("há", "Há"),
-            };
-          }
-        );
+        
+        // Formatar as datas para "ha x tempo" usando dados locais
+        const formattedArticles = newsData.articles.map((article) => ({
+          ...article,
+          publishedAt: formatTimeAgo(article.publishedAt),
+        }));
 
         setNews(formattedArticles); // Fallback para dados locais
       } finally {
@@ -124,11 +91,16 @@ export default function Index() {
     fetchNews();
   }, [fetchNews]);
 
+  // Função chamada pelo RefreshControl
+  const onRefresh = useCallback(() => {
+    fetchNews(true);
+  }, [fetchNews]);
+
   const handleSearchSubmit = useCallback(
     (query: string) => {
       console.log("Pesquisa submetida:", query);
-      router.push({ pathname: "/searchResults", params: { query } });
       dispatch(setQuery(query));
+      router.push({ pathname: "/searchResults" });
     },
     [router]
   );
@@ -167,97 +139,9 @@ export default function Index() {
     rightDrawerRef.current?.closeDrawer();
   }, []);
 
-  // Função para renderizar cada item da lista
-  const renderNewsItem = useCallback(
-    ({ item }: { item: ArticleParams }) => (
-      <NewsCard
-        key={item.url}
-        title={item.title}
-        bodyText={item.description}
-        image={item.image}
-        sourceName={item.source.name}
-        time={item.publishedAt}
-        onPress={() => handleCardPress(item)}
-      />
-    ),
-    [handleCardPress]
-  );
-
-  // Função chamada pelo RefreshControl
-  const onRefresh = useCallback(() => {
-    fetchNews(true);
-  }, [fetchNews]);
-
   const handlePresentAboutUsModal = useCallback(() => {
     aboutUsRef.current?.present();
   }, []);
-
-  const ItemSeparator = () => (
-    <View
-      style={[
-        styles.separator,
-        {
-          backgroundColor: themeColors.borderColor,
-        },
-      ]}
-    />
-  );
-
-  const ListHeader = () => (
-    <>
-      {/* Search Bar */}
-      <SearchBar
-        style={styles.searchBar}
-        onSubmitEditing={handleSearchSubmit}
-      />
-      {/* Carrousel */}
-      <View style={styles.carrousel}>
-        <View style={styles.carrouselTextWrapper}>
-          <TitleText style={[styles.carrouselText, { fontSize: 18 }]}>
-            🔥 Em alta
-          </TitleText>
-          <MaterialCommunityIcons
-            name="arrow-right"
-            style={[
-              styles.carrouselText,
-              { color: themeColors.text, fontSize: 22 },
-            ]}
-          />
-        </View>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          scrollToOverflowEnabled={true}
-        >
-          {loading ? (
-            // Mostrar Skeletons durante o carregamento
-            <>
-              <HighlightCardSkeleton />
-              <HighlightCardSkeleton />
-              <HighlightCardSkeleton />
-              <HighlightCardSkeleton />
-              <HighlightCardSkeleton />
-            </>
-          ) : (
-            // Mostrar Cards reais após carregar
-            news &&
-            news.length > 0 &&
-            news.slice(0, 5).map((item) => {
-              return (
-                <HighlightCard
-                  key={item.url}
-                  description={item.description}
-                  image={item.image}
-                  onPress={() => handleCardPress(item)}
-                />
-              );
-            })
-          )}
-        </ScrollView>
-      </View>
-      <TitleText style={styles.listTitle}>🗞️ Últimas Notícias</TitleText>
-    </>
-  );
 
   return (
     <>
@@ -275,7 +159,6 @@ export default function Index() {
         }}
       />
       <View style={{ flex: 1 }}>
-        <StatusBar style="auto" />
         <ReanimatedDrawerLayout
           drawerWidth={drawerWidth}
           ref={leftDrawerRef}
@@ -302,39 +185,25 @@ export default function Index() {
                 onMenuPress={handleMenuPress}
                 onAccountPress={handleAccountPress}
               />
-              {loading && !isRefreshing ? (
-                <FlatList
-                  data={[1, 2, 3]}
-                  renderItem={() => (
-                    <NewsCardSkeleton style={{ marginVertical: 60 }} />
-                  )}
-                  keyExtractor={(item) => item.toString()}
-                  ListHeaderComponent={ListHeader}
-                  contentContainerStyle={{ gap: 20 }}
-                  keyboardShouldPersistTaps="handled"
-                />
-              ) : (
-                <FlatList
-                  data={news}
-                  renderItem={renderNewsItem}
-                  keyExtractor={(item) => item.url}
-                  ListHeaderComponent={ListHeader}
-                  keyboardShouldPersistTaps="handled"
-                  keyboardDismissMode="on-drag"
-                  ItemSeparatorComponent={ItemSeparator}
-                  refreshControl={
-                    <RefreshControl
-                      refreshing={isRefreshing}
-                      onRefresh={onRefresh}
-                      colors={["rgb(29, 108, 122)"]} // Cor do indicador no Android
-                    />
-                  }
-                />
-              )}
+              <NewsList
+                data={news}
+                loading={loading}
+                isRefreshing={isRefreshing}
+                onRefresh={onRefresh}
+                onPressItem={handleCardPress}
+                ListHeaderComponent={() => (
+                  <NewsListHeader
+                    data={news}
+                    loading={loading}
+                    handleSearchSubmit={handleSearchSubmit}
+                  />
+                )}
+              />
             </View>
             <AboutUsModal ref={aboutUsRef} appVersion={appVersion} />
           </ReanimatedDrawerLayout>
         </ReanimatedDrawerLayout>
+        <StatusBar style="auto" />
       </View>
     </>
   );
@@ -343,32 +212,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  searchBar: {
-    marginTop: 40,
-    alignSelf: "center",
-    width: "90%",
-  },
-  carrousel: {
-    marginTop: 60,
-    marginBottom: 90,
-  },
-  carrouselTextWrapper: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  carrouselText: {
-    fontSize: 20,
-    marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  listTitle: {
-    marginHorizontal: 20,
-  },
-  separator: {
-    height: 1,
-    marginHorizontal: 15,
-    marginVertical: 15,
-  },
+  }
 });
