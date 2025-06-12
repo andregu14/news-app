4a563cca506947c024c8cb20e5466e01c6871e75
André Gustavo <andregustavo147@gmail.com>
2025-06-12 17:20:46 -0300

    refactor: better error handling, simplified jsx

--- app/searchResults.tsx
+++ app/searchResults.tsx
@@ -15,22 +15,28 @@ import ReanimatedDrawerLayout, {
   DrawerPosition,
   DrawerLayoutMethods,
 } from "react-native-gesture-handler/ReanimatedDrawerLayout";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import AccountSideMenu from "@/components/AccountSideMenu";
 import SideMenu from "@/components/SideMenu";
 import { ArticleParams } from "@/constants/NewsData";
-import { Image } from "expo-image";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import AboutUsModal from "@/components/AboutUsModal";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "@/store";
 import NewsListHeader from "@/components/NewsListHeader";
 import NewsList from "@/components/NewsList";
-import { fetchSearchNewsAsync } from "@/store/newsSlice";
+import {
+  fetchMoreSearchNewsAsync,
+  fetchSearchNewsAsync,
+  resetErrorMessage,
+} from "@/store/newsSlice";
 import { setQuery } from "@/store/searchSlice";
+import { Image } from "expo-image";
+import Toast from "@/components/ToastMessage";
 
 const { width: screenWidth } = Dimensions.get("window");
 const drawerWidth = screenWidth * 0.8;
+const PRESS_COOLDOWN = 1000;
 
 export default function SearchResults() {
   const insets = useSafeAreaInsets();
@@ -40,28 +46,30 @@ export default function SearchResults() {
   const leftDrawerRef = useRef<DrawerLayoutMethods>(null);
   const rightDrawerRef = useRef<DrawerLayoutMethods>(null);
   const aboutUsRef = useRef<BottomSheetModal>(null);
-  
-  // USAR APENAS O ESTADO GLOBAL DE SEARCH
   const searchQuery = useSelector((state: RootState) => state.search.query);
-  const { articles: news, loading, error } = useSelector(
-    (state: RootState) => state.news.searchResults
-  );
+  const {
+    articles: news,
+    loading,
+    error,
+    hasMore,
+    loadingMore,
+    errorMessage,
+  } = useSelector((state: RootState) => state.news.searchResults);
+  const lastPressTimeRef = useRef(0);
 
   const appVersion = require("../app.json").expo.version;
 
   // Função para buscar notícias
-  const fetchNews = useCallback(async (searchTerm: string) => {
-    if (!searchTerm.trim()) return;
-    
-    console.log('Buscando notícias para:', searchTerm);
-    
-    try {
-      await dispatch(fetchSearchNewsAsync(searchTerm)).unwrap();
-    } catch (e) {
-      console.error(error);
-    }
-  }, [dispatch]);
+  const fetchNews = useCallback(
+    (searchTerm: string) => {
+      if (!searchTerm.trim()) return;
+      console.log("Buscando notícias para:", searchTerm);
+      dispatch(fetchSearchNewsAsync(searchTerm));
+    },
+    [dispatch]
+  );
 
+  // Verifica se searchQuery mudou para fazer nova busca
   useEffect(() => {
     if (searchQuery?.trim()) {
       rightDrawerRef.current?.closeDrawer();
@@ -69,12 +77,80 @@ export default function SearchResults() {
     }
   }, [searchQuery, fetchNews]);
 
+  // Função para carregar mais items
+  const handleLoadMore = useCallback(() => {
+    console.log("Fim da lista atingida buscando mais noticias");
+    if (hasMore && !loadingMore) {
+      dispatch(fetchMoreSearchNewsAsync());
+    }
+  }, [dispatch, hasMore, loadingMore]);
+
+  // Função de retry
+  const handleRetry = useCallback(() => {
+    if (searchQuery) {
+      fetchNews(searchQuery);
+    }
+  }, [fetchNews, searchQuery]);
+
   // Função para submeter nova busca
   const handleSearchSubmit = useCallback(
     (query: string) => {
       dispatch(setQuery(query));
     },
     [dispatch]
+  );
+
+  // Mostra um toast onError
+  useEffect(() => {
+    if (errorMessage) {
+      Toast.show({
+        type: "error",
+        text1: errorMessage as string,
+        autoHide: false,
+        position: "bottom",
+        bottomOffset: insets.bottom + 50,
+        onPress: () => {
+          Toast.hide();
+        },
+      });
+      dispatch(resetErrorMessage());
+    }
+  }, [errorMessage, dispatch]);
+
+  // Componente para lista vazia
+  const EmptyComponent = useMemo(
+    () => (
+      <View
+        style={[styles.centerContainer, { marginBottom: insets.bottom * 2 }]}
+      >
+        <Text style={styles.centerText}>
+          Nenhum resultado encontrado para{" "}
+          <Text style={styles.queryText}>"{searchQuery}"</Text>
+        </Text>
+        <Image
+          style={styles.image}
+          source={require("@/assets/images/not_found.png")}
+          contentFit="contain"
+          alt="Nenhuma notícia encontrada"
+        />
+        <Text style={[styles.subText, { color: themeColors.secondaryText }]}>
+          Tente buscar por outros termos.
+        </Text>
+      </View>
+    ),
+    [searchQuery]
+  );
+
+  // Componente de header da lista e memorizado
+  const listHeader = useMemo(
+    () => (
+      <NewsListHeader
+        showCarrousel={false}
+        showTitle={false}
+        handleSearchSubmit={handleSearchSubmit}
+      />
+    ),
+    [handleSearchSubmit]
   );
 
   // Funções para controlar os menus
@@ -87,101 +163,34 @@ export default function SearchResults() {
   };
 
   // Função para navegar para os detalhes da notícia
-  const handleCardPress = useCallback(
-    (item: ArticleParams) => {
-      console.log("Navegando para detalhes do item:", item.title);
-      router.push({
-        pathname: "/newsDetails",
-        params: {
-          newsTitle: encodeURIComponent(item.title),
-          newsDescription: encodeURIComponent(item.description),
-          newsContent: item.content,
-          newsUrl: encodeURIComponent(item.url),
-          newsImage: encodeURIComponent(item.image),
-          newsPublishedAt: encodeURIComponent(item.publishedAt),
-          newsSourceName: encodeURIComponent(item.source.name),
-          newsSourceUrl: encodeURIComponent(item.source.url),
-        },
-      });
-    },
-    []
-  );
+  const handleCardPress = useCallback((item: ArticleParams) => {
+    // prevenção de múltiplos toques
+    const now = Date.now();
+    if (now - lastPressTimeRef.current < PRESS_COOLDOWN) {
+      return;
+    }
+    lastPressTimeRef.current = now;
 
+    console.log("Navegando para detalhes do item:", item.title);
+    router.push({
+      pathname: "/newsDetails",
+      params: {
+        newsTitle: item.title,
+        newsDescription: item.description,
+        newsContent: item.content,
+        newsUrl: item.url,
+        newsImage: item.image,
+        newsPublishedAt: item.publishedAt,
+        newsSourceName: item.source.name,
+        newsSourceUrl: item.source.url,
+      },
+    });
+  }, []);
+
+  // Função para mostrar modal
   const handlePresentAboutUsModal = useCallback(() => {
     aboutUsRef.current?.present();
   }, []);
-
-  const ShowResults = () => (
-    <View style={{ flex: 1 }}>
-      {loading ? (
-        <>
-          <NewsListHeader
-            showCarrousel={false}
-            showTitle={false}
-            defaultSearchValue={searchQuery}
-          />
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator size={"large"} />
-          </View>
-        </>
-      ) : error ? (
-        <View style={{ flex: 1 }}>
-          <NewsListHeader
-            showCarrousel={false}
-            showTitle={false}
-            handleSearchSubmit={handleSearchSubmit}
-            defaultSearchValue={searchQuery}
-          />
-          <View style={styles.notFoundContainer}>
-            <Text style={styles.errorText}>{error}</Text>
-            <Image
-              style={styles.notFoundImage}
-              source={require("@/assets/images/not_found.png")}
-              contentFit="cover"
-              alt="Erro ao buscar notícias"
-            />
-          </View>
-        </View>
-      ) : news.length > 0 ? (
-        <NewsList
-          data={news}
-          loading={loading}
-          isRefreshing={loading}
-          onPressItem={handleCardPress}
-          ListHeaderComponent={() => (
-            <NewsListHeader
-              showCarrousel={false}
-              showTitle={false}
-              searchBarStyle={{ marginBottom: 35 }}
-              handleSearchSubmit={handleSearchSubmit}
-              defaultSearchValue={searchQuery}
-            />
-          )}
-        />
-      ) : (
-        <View style={{ flex: 1 }}>
-          <NewsListHeader
-            showCarrousel={false}
-            showTitle={false}
-            handleSearchSubmit={handleSearchSubmit}
-            defaultSearchValue={searchQuery}
-          />
-          <View style={styles.notFoundContainer}>
-            <Text style={styles.notFoundText}>
-              Nenhuma notícia encontrada para
-              <Text style={styles.queryText}> "{searchQuery}"</Text>
-            </Text>
-            <Image
-              style={styles.notFoundImage}
-              source={require("@/assets/images/not_found.png")}
-              contentFit="cover"
-              alt="Nenhuma noticia encontrada"
-            />
-          </View>
-        </View>
-      )}
-    </View>
-  );
 
   return (
     <>
@@ -191,8 +200,6 @@ export default function SearchResults() {
           position: "absolute",
           backgroundColor: themeColors.headerBackground,
           height: insets.top,
-          top: 0,
-          bottom: 0,
           left: 0,
           right: 0,
           zIndex: 999,
@@ -218,15 +225,42 @@ export default function SearchResults() {
             renderNavigationView={() => <SideMenu />}
             drawerContainerStyle={{ marginTop: insets.top }}
           >
-            <View style={[styles.content]}>
-              {/* Header */}
+            <View style={[styles.content, { paddingTop: insets.top }]}>
               <Header
-                style={{ marginTop: insets.top }}
                 onMenuPress={handleMenuPress}
                 onAccountPress={handleAccountPress}
               />
-              <ShowResults />
+              <NewsList
+                data={news}
+                loading={loading}
+                error={error}
+                showError={error ? true : false}
+                onRetry={handleRetry}
+                onPressItem={handleCardPress}
+                onEndReached={handleLoadMore}
+                refreshControl={false}
+                ListHeaderComponent={listHeader}
+                ListEmptyComponent={EmptyComponent}
+                ListFooterComponent={
+                  loadingMore ? (
+                    <ActivityIndicator
+                      style={{ marginVertical: 20 }}
+                      size="small"
+                      color={themeColors.mainColor}
+                    />
+                  ) : null
+                }
+              />
             </View>
+            <View
+              style={[
+                styles.bottomView,
+                {
+                  height: insets.bottom,
+                  backgroundColor: themeColors.background,
+                },
+              ]}
+            />
             <AboutUsModal ref={aboutUsRef} appVersion={appVersion} />
           </ReanimatedDrawerLayout>
         </ReanimatedDrawerLayout>
@@ -240,39 +274,36 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
   },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 180,
-  },
-  notFoundContainer: {
+  centerContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 20,
-    marginBottom: 180,
   },
-  notFoundText: {
+  centerText: {
     fontSize: 18,
     textAlign: "center",
-    marginBottom: 20,
+    marginBottom: 8,
     fontWeight: "500",
+  },
+  subText: {
+    fontSize: 14,
+    textAlign: "center",
   },
   queryText: {
     fontWeight: "bold",
     color: "rgb(29, 108, 122)",
   },
-  notFoundImage: {
-    width: 200,
-    height: 200,
-    alignSelf: "center",
-  },
-  errorText: {
-    fontSize: 18,
-    textAlign: "center",
+  image: {
+    width: 150,
+    height: 150,
     marginBottom: 20,
-    color: "red",
-    fontWeight: "500",
+  },
+  bottomView: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
   },
 });
