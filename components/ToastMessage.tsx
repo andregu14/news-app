9a62f396c878cd158b9bddd66664393798bb205c
André Gustavo <andregustavo147@gmail.com>
2025-06-13 22:53:30 -0300

    feat: new toast styles

--- components/ToastMessage.tsx
+++ components/ToastMessage.tsx
@@ -1,4 +1,4 @@
-import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import Toast, { ToastConfig } from "react-native-toast-message";
 
@@ -37,22 +37,65 @@ export const toastConfig: ToastConfig = {
       </View>
     </Pressable>
   ),
+  // Toast para quando um item é ADICIONADO aos favoritos
+  favorite_added: ({ text1, text2, onPress }) => (
+    <Pressable onPress={onPress}>
+      <View style={[styles.container, styles.favoriteAddedContainer]}>
+        <Ionicons name="heart" size={24} color="white" />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{text1}</Text>
+          {text2 && <Text style={styles.message}>{text2}</Text>}
+        </View>
+        <MaterialIcons
+          name="close"
+          size={20}
+          color="white"
+          style={styles.closeIcon}
+        />
+      </View>
+    </Pressable>
+  ),
+
+  // Toast para quando um item é REMOVIDO dos favoritos
+  favorite_removed: ({ text1, text2, onPress }) => (
+    <Pressable onPress={onPress}>
+      <View style={[styles.container, styles.favoriteRemovedContainer]}>
+        <Ionicons name="heart-dislike-outline" size={24} color="white" />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{text1}</Text>
+          {text2 && <Text style={styles.message}>{text2}</Text>}
+        </View>
+        <MaterialIcons
+          name="close"
+          size={20}
+          color="white"
+          style={styles.closeIcon}
+        />
+      </View>
+    </Pressable>
+  ),
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 15,
     borderRadius: 12,
-    margin: 10,
+    marginHorizontal: 10,
     flexDirection: "row",
     alignItems: "center",
     elevation: 5,
   },
   errorContainer: {
-    backgroundColor: "#FF3B30",
+    backgroundColor: "#FF3B30", // Vermelho
   },
   successContainer: {
-    backgroundColor: "#34C759",
+    backgroundColor: "#34C759", // Verde
+  },
+  favoriteAddedContainer: {
+    backgroundColor: "#007AFF", // Azul
+  },
+  favoriteRemovedContainer: {
+    backgroundColor: "#8E8E93", // Cinza
   },
   textContainer: {
     marginLeft: 10,
